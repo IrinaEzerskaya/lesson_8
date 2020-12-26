119abbac341dbcd309b1cf52396e19d432d1860b
Irina Ezerskaya <titira217@mail.ru>
2020-12-26 14:04:34 +0300

    Initial commit

---
+++ src/com/Exception/Main.java
@@ -0,0 +1,21 @@
+package com.Exception;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        String s = null;
+
+        // Раскоментарить для вызова исключения java.lang.NullPointerException
+        //int len = s.length();
+
+        String[] arr = {"Вася","Петя","Коля"};
+
+        // Раскоментарить для вызова исключения java.lang.ArrayIndexOutOfBoundsException
+        //System.out.println(arr[3]);
+
+        // Раскоментарить для вызова исключения java.lang.Exception
+        //throw new Exception("Exception message");
+    }
+
+}
